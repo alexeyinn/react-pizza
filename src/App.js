a2e103f8182e35229d9a9598416bb01b3c30700b
alexeyinn <69390951+alexeyinn@users.noreply.github.com>
2020-11-22 20:24:01 +0300

    Категории в React

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./styles.css";
-import { Header } from "./components";
+import { Header, Categories } from "./components";
 
 export default function App() {
   return (
@@ -9,16 +9,7 @@ export default function App() {
       <div className="content">
         <div className="container">
           <div className="content__top">
-            <div className="categories">
-              <ul>
-                <li className="active">Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
-              </ul>
-            </div>
+            <Categories />
             <div className="sort">
               <div className="sort__label">
                 <svg
