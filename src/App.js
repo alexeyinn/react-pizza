f2d176444be6437fcdca563bd4a8303ba9728ab6
alexeyinn <69390951+alexeyinn@users.noreply.github.com>
2020-12-09 20:42:23 +0300

    Из db прокидываем все данные в pizzaBlock

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import "./styles.css";
 import { Header } from "./components";
 import { Home, Cart } from "./pages";
 import { Route } from "react-router-dom";
+import axios from "axios";
 
 export default function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    axios.get("https://r4eei.csb.app/db.json").then(({ data }) => {
+      setPizzas(data.pizzas);
+    });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" render={() => <Home items={pizzas} />} />
         <Route exact path="/cart" component={Cart} />
       </div>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Categories, SortPopup, PizzaBlock } from "../components";
 
-export default function Home() {
+export default function Home({ items }) {
   return (
     <div className="container">
       <div className="content__top">
@@ -13,7 +13,9 @@ export default function Home() {
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
-        <PizzaBlock />
+        {items.map((obj) => (
+          <PizzaBlock key={obj.id} {...obj} />
+        ))}
       </div>
     </div>
   );
