635f8f778a5cb13a61652a5dfb568170f3ed4378
alexeyinn <69390951+alexeyinn@users.noreply.github.com>
2020-12-19 20:00:01 +0300

    Redux-hooks Урок 7, 0:55:00

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import { Header } from "./components";
 import { Home, Cart } from "./pages";
 import { Route } from "react-router-dom";
 import axios from "axios";
+import { useSelector, useDispatch } from "react-redux";
+import { setPizzas } from "./redux/actions/pizzas";
 
 export default function App() {
-  const [pizzas, setPizzas] = React.useState([]);
+  const dispatch = useDispatch();
+  const { items } = useSelector(({ pizzas, filters }) => {
+    return {
+      items: pizzas.items
+    };
+  });
 
   React.useEffect(() => {
     axios.get("https://r4eei.csb.app/db.json").then(({ data }) => {
-      setPizzas(data.pizzas);
+      dispatch(setPizzas(data.pizzas));
     });
   }, []);
 
@@ -18,7 +25,7 @@ export default function App() {
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route exact path="/" render={() => <Home items={pizzas} />} />
+        <Route exact path="/" render={() => <Home items={items} />} />
         <Route exact path="/cart" component={Cart} />
       </div>
     </div>
