fe36158e6a13f18221c996885a4dbe03d5f56e76
alexeyinn <69390951+alexeyinn@users.noreply.github.com>
2020-12-20 20:02:02 +0300

    Подготовка к созданию функционирующих фильтрации и сортировки

--- src/App.js
+++ src/App.js
@@ -4,28 +4,23 @@ import { Header } from "./components";
 import { Home, Cart } from "./pages";
 import { Route } from "react-router-dom";
 import axios from "axios";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import { setPizzas } from "./redux/actions/pizzas";
 
 export default function App() {
   const dispatch = useDispatch();
-  const { items } = useSelector(({ pizzas, filters }) => {
-    return {
-      items: pizzas.items
-    };
-  });
 
   React.useEffect(() => {
     axios.get("https://r4eei.csb.app/db.json").then(({ data }) => {
       dispatch(setPizzas(data.pizzas));
     });
-  }, []);
+  }); //Если возникунут проблемы с получением данных из БД, добавить в конце ,[]
 
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route exact path="/" render={() => <Home items={items} />} />
+        <Route exact path="/" component={Home} />
         <Route exact path="/cart" component={Cart} />
       </div>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,34 @@
 import React from "react";
 import { Categories, SortPopup, PizzaBlock } from "../components";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { setCategory } from "../redux/actions/filters";
 
-export default function Home({ items }) {
+const categoryNames = [
+  "Мясные",
+  "Вегетарианская",
+  "Гриль",
+  "Острые",
+  "Закрытые"
+];
+const sortItems = [
+  { name: "популярности", type: "popular" },
+  { name: "цене", type: "price" },
+  { name: "алфавиту", type: "ABC" }
+];
+
+export default function Home() {
+  const onSelectCategory = React.useCallback((index) => {
+    dispatch(setCategory(index));
+  }, []);
+  const dispatch = useDispatch();
+  const items = useSelector(({ pizzas }) => pizzas.items);
+
   return (
     <div className="container">
       <div className="content__top">
-        <Categories
-          //onClickItem={(name) => console.log(name)}
-          items={["Мясные", "Вегетарианская", "Гриль", "Острые", "Закрытые"]}
-        />
-        <SortPopup
-          items={[
-            { name: "популярности", type: "popular" },
-            { name: "цене", type: "price" },
-            { name: "алфавиту", type: "ABC" }
-          ]}
-        />
+        <Categories onClickItem={onSelectCategory} items={categoryNames} />
+        <SortPopup items={sortItems} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
